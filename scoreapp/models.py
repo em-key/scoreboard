1dd345ed62fe8d47a0805ccb028330f13985d7dd
em-key <mk1091@web.de>
2021-01-02 21:55:30 +0100

    preparations to dockerize

--- scoreapp/models.py
+++ scoreapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    mail = models.EmailField(max_length=254)
+    mail = models.EmailField(max_length=254, blank=True)
 
     def __str__(self):
         return self.name
--- scoreboard/settings_prod.py
+++ scoreboard/settings_prod.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,4 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/home/markus/DevProjects/teststatic'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+SILENCED_SYSTEM_CHECKS = ["security.W004","security.W008","security.W012","security.W016"]
