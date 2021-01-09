59eb85ed80cbac1d4e289d9aaa81c8a93cc2af98
em-key <835946+em-key@users.noreply.github.com>
2021-01-09 15:31:19 +0100

    update Dockerfile; update mqtt script with logging, api webserver settings

--- build_and_run/start-server.sh
+++ build_and_run/start-server.sh
@@ -13,7 +13,23 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py initadminuser
 
-# Run Server
+# Run MQTT Client
+
+if [ "$MQTT" = "yes" ]
+then
+    echo 'Start MQTT Client - send channel updates to scoreboard/player1 or scoreboard/player2'
+    python scoreboard_mqtt/scoreboard_mqtt.py &
+fi
+
+# Run RESTAPI
+
+if [ "$API" = "yes" ]
+then
+    echo 'Start REST API - connect to swagger UI via exposed port 8080 /v1/ui'
+    gunicorn --chdir ./scoreboard_api scoreboard_rest --bind 127.0.0.1:8020 --workers 3 --daemon
+fi
+
+# Run Webapp
 # python manage.py runserver 0.0.0.0:8000
-gunicorn scoreboard.wsgi --user nginx --bind 0.0.0.0:8010 --workers 3 &
+gunicorn scoreboard.wsgi --user nginx --bind 127.0.0.1:8010 --workers 3 --daemon
 nginx -g "daemon off;"
